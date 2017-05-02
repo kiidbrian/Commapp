70a25cc4f01a40a67dcac9db3fd3ca639183b1e7
David Komlah <david@smsgh.com>
2017-05-02 01:10:03 +0000

    setup project structure

--- app/src/main/java/com/ecoach/app/commapp/Activities/LoginActivity.java
+++ app/src/main/java/com/ecoach/app/commapp/Activities/LoginActivity.java
@@ -1,13 +1,15 @@
-package com.ecoach.app.commapp;
+package com.ecoach.app.commapp.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ecoach.app.commapp.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
