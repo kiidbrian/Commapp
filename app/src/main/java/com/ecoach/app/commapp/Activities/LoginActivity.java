6baef662db80596f95e23cc4e287f3242a25b8d5
David Komlah <david@smsgh.com>
2017-05-19 06:27:29 +0000

    Login

--- app/src/main/java/com/ecoach/app/commapp/Activities/LoginActivity.java
+++ app/src/main/java/com/ecoach/app/commapp/Activities/LoginActivity.java
@@ -1,15 +1,17 @@
 package com.ecoach.app.commapp.Activities;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.ecoach.app.commapp.R;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //getActionBar().hide();
     }
 }
